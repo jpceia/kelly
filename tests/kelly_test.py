67494dc5e5d5253b3bdec5410ac932c4fc761e12
jpceia <joao.p.ceia@gmail.com>
2019-05-26 22:38:31 +0100

    added kelly_test.py, with a test for the Bernoulli function

---
+++ tests/kelly_test.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+from kelly import bernoulli
+
+class Test(unittest.TestCase):
+
+    def test_bernoulli(self):
+        x = bernoulli(3, .5)
+        self.assertAlmostEqual(x, 0.25)
+
+        x = bernoulli(3, .5, .1)
+        self.assertAlmostEqual(x, 0.19463667820069203)
+
+        x = bernoulli(5, .33)
+        self.assertAlmostEqual(x, 0.1625)
+
+        x = bernoulli(5, .33, .1)
+        self.assertAlmostEqual(x, 0.10515584316128047)
+
+
+if __name__ == "__main__":
+    unittest.main()
