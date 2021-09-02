2d24e633f7d68ee943fbb7f88f945c1a657d32d5
Joao Ceia <joao.p.ceia@gmail.com>
2021-09-02 21:43:02 +0100

    Fixed simple kelly criterion tests.

--- tests/kelly_test.py
+++ tests/kelly_test.py
@@ -4,17 +4,17 @@ from kelly import bernoulli
 class Test(unittest.TestCase):
 
     def test_bernoulli(self):
-        x = bernoulli(3, .5)
+        x = bernoulli(3, 0.5)
         self.assertAlmostEqual(x, 0.25)
 
-        x = bernoulli(3, .5, .1)
-        self.assertAlmostEqual(x, 0.19463667820069203)
+        x = bernoulli(3, 0.5, err=0.1)
+        self.assertAlmostEqual(x, 0.1838235294117647)
 
-        x = bernoulli(5, .33)
+        x = bernoulli(5, 0.33)
         self.assertAlmostEqual(x, 0.1625)
 
-        x = bernoulli(5, .33, .1)
-        self.assertAlmostEqual(x, 0.10515584316128047)
+        x = bernoulli(5, 0.33, err=0.1)
+        self.assertAlmostEqual(x, 0.10209107806691455)
 
 
 if __name__ == "__main__":
