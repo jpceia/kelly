9308b017337515a6be04959e956be65163e325e4
Joao Ceia <joao.p.ceia@gmail.com>
2021-09-04 00:41:57 +0100

    updated values of kelly exclusive test

--- tests/exclusive_test.py
+++ tests/exclusive_test.py
@@ -1,10 +1,11 @@
 import unittest
 import numpy as np
-from kelly import exclusive
+import sys; sys.path.insert(0, "..")
+from kelly import exclusive, exclusive_exp
 
 class Test(unittest.TestCase):
 
-    def test_exclusive1(self):
+    def test_exclusive(self):
         p = np.array([0.25, .40, 0.35])
         q = np.array([0.23, .40, 0.37])
 
@@ -14,12 +15,12 @@ class Test(unittest.TestCase):
         
         o = 1 / q
         b = exclusive(o, p)
-        b_expected = [0.02597403, 0.01038961, 0.]
+        b_expected = [0.03243243243243242, 0.021621621621621623, 0.]
         np.testing.assert_allclose(b, b_expected, atol=1e-8)
 
         o = 0.95 / q
         b = exclusive(o, p)
-        b_expected = [0.00789474, 0., 0.]
+        b_expected = [0.010416666666666614, 0., 0.]
         np.testing.assert_allclose(b, b_expected, atol=1e-8)
 
         # zero investment
